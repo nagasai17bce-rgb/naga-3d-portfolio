60f0ca102fa324f1caa58bf3869e3a42cee3cebc
nagasai17bce-rgb <nagasai17bce@gmail.com>
2026-09-24 21:30:20 +0530

    Update Scene.tsx

--- Scene.tsx
+++ Scene.tsx
@@ -113,7 +113,9 @@ function Avatar3D() {
     <group ref={group} position={[3, .55, -.35]}>
       <Float speed={1.25} rotationIntensity={.1} floatIntensity={.2}>
         {[.08, .03, 0].map((z, i) => (
-          <DreiImage key={i} url="/naga-portrait.png" transparent opacity={i === 2 ? .96 : .12} scale={[2.2 + i * .06, 2.82 + i * .08} position={[i * .025, i * .018, z]} />
+          <group key={i} position={[i * .025, i * .018, z]}>
+            <DreiImage url="/naga-portrait.png" transparent opacity={i === 2 ? .96 : .12} scale={[2.2 + i * .06, 2.82 + i * .08]} />
+          </group>
         ))}
         <mesh position={[0, 0, -.05]}>
           <planeGeometry args={[2.5, 3.18]} />
